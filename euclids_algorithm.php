f58ae34ec91dbac23678a97ad843d9e9285040ca
Jonathan Sudlow <jsudlow@gmail.com>
2012-03-22 00:42:51 -0500

    first commit and example

---
+++ euclids_algorithm.php
@@ -0,0 +1,41 @@
+<?
+
+//Euclids Algorithm in PHP
+
+//start with the remainder is one, the opposite of the terminating condition
+
+
+//we use hard coded values for M and N 
+
+
+function get_gcd($a, $b) {
+  $r = 1;
+$m = $a;
+$n = $b;
+$counter = 0;
+
+
+while($r != 0) {
+  $counter = $counter + 1;
+  
+  $r = $m % $n;
+  echo "Remainder: " . $r . " iteration ". $counter . "\n";
+
+  if($r != 0) {
+    $m = $n;
+    $n = $r;
+
+    }
+
+  echo "M  " . $m . "iteration " . $counter . "\n";
+  echo "N " . $n . "iteration " . $counter . "\n";
+    }
+
+
+print "LCD IS " . $n . "\n"; 
+
+}
+
+get_gcd(6,8);
+
+?>
